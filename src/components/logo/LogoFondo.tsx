7069f2d58cb69c79291f1b70581cd7018190545f
devopsandes <devops@andessalud.ar>
2025-06-05 16:03:41 +0200

    fix import logo

--- src/components/logo/LogoFondo.tsx
+++ src/components/logo/LogoFondo.tsx
@@ -1,11 +1,12 @@
 import './logoFondo.css';
+import logo from '../../assets/logo-sol.png'
 
 const LogoFondo = () => {
   return (
     <div className="container-logo">
         <div className='franja-cel'></div>
         <div className='logo-cel'>
-            <img src="/src/assets/logo-sol.png" alt="logo-sol" width={200} height={200} />
+            <img src={logo} alt="logo-sol" width={200} height={200} />
         </div>
         <div className='franja-cel'></div>
     </div>
