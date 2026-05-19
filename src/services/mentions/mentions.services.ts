aba90d90f6825acf013cf00199688db75d14d2f2
federico <federicolopez0696@gmail.com>
2026-05-19 09:26:50 -0300

    npx tsc -b

--- src/services/mentions/mentions.services.ts
+++ src/services/mentions/mentions.services.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { ErrorResponse } from "../../interfaces/auth.interface"
 import { perfTrackRequest } from "../../utils/perfTracker"
-import { resolveClient } from "../apiClient"
 
 export type MentionChatItem = {
   chatId: string
